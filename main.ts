43e515350db65fd0f3dd40217ebba99f38b3df22
tymongiang <106290337+tymongiang@users.noreply.github.com>
2022-05-26 11:39:55 +0930

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+for (let index = 0; index < 4; index++) {
+    basic.showString("DADDY")
+    music.playMelody("C5 B A F E A D C ", 120)
+    led.plotBarGraph(
+    1,
+    100
+    )
+}
+basic.forever(function () {
+	
+})
